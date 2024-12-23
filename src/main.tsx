5bc08c94b3fab90f5c7183be41710bbad65fc9d5
chanhk-im <cfasd1875@gmail.com>
2024-12-23 17:24:10 +0900

    [CH] feat[#8]: 테스트용입니다.

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import App from './App.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    {/* 나는 빡빡이다!!!! */}
+    {/* 나는 빡빡이다!!!!#8 */}
   </StrictMode>,
 );
