9ed37e6613fb5b91572b14ace039568fe8a8f9bd
JangSiYoung <chsy1894@handong.ac.kr>
2024-12-27 14:41:57 +0900

    [SY] feat[#28]:소개,리크루팅 수정

--- src/pages/Intro/IntroPage.tsx
+++ src/pages/Intro/IntroPage.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import './IntroPage.css';
 
 export default function IntroPage() {
-  return <div>소개 페이지</div>;
+  return (
+    <div className="intro-page">
+      <div className="intro-section">
+        <p>Visual Image</p>
+        <p className="apply-button">
+          <button>
+            <Link to="/recruit">지원하기</Link>
+          </button>
+        </p>
+      </div>
+      <div className="intro-section">
+        <p>동아리 소개</p>
+      </div>
+      <div className="intro-section">
+        <p>동아리 프로젝트 소개</p>
+      </div>
+    </div>
+  );
 }
--- src/pages/Recruit/RecruitPage.tsx
+++ src/pages/Recruit/RecruitPage.tsx
@@ -1,6 +1,39 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import './RecruitPage.css';
 
 export default function RecruitPage() {
-  return <div>리크루팅 페이지</div>;
+  return (
+    <form action="" method="POST">
+      <div className="recruit-page">
+        <p>25-1 Recruit</p>
+        <div className="recruit-section">
+          <label htmlFor="name">이름</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your name" /*required*/
+          />
+        </div>
+        <div className="recruit-section">
+          <label htmlFor="studentnumber">학번</label>
+          <input type="text" id="studentnumber" /*required*/ />
+        </div>
+        <div className="recruit-section">
+          <label htmlFor="email">이메일</label>
+          <input type="text" id="email" /*required*/ />
+        </div>
+        <div className="recruit-section">
+          <label htmlFor="Motivation">지원동기</label>
+          <input type="text" id="Motivation" /*required*/ />
+        </div>
+        <div className="recruit-section">
+          <label htmlFor="experience">프로젝트 경험</label>
+          <input type="text" id="experience" />
+          <p>
+            <input type="submit" />
+          </p>
+        </div>
+      </div>
+    </form>
+  );
 }
