16315dc0777ec220c6d16124e9a2a42c6bd97e62
chanhk-im <cfasd1875@gmail.com>
2024-12-30 22:56:05 +0900

    [CH] fix[#35]: Board CRUD: Delete 할 때 List에 변경사항 바로 업데이트 안되는 버그 FIX

--- src/components/Board/Delete/BoardDelete.tsx
+++ src/components/Board/Delete/BoardDelete.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteBoards } from '../../../api/board';
 import './BoardDelete.css';
+import { QUERY_KEY } from '~/api/queryKey';
 
-export default function BoardDelete({ id }: { id: number }) {
+export default function BoardDelete({
+  id,
+  category,
+}: {
+  id: number;
+  category: number;
+}) {
   const queryClient = useQueryClient();
 
   const deleteMutation = useMutation({
@@ -11,7 +18,12 @@ export default function BoardDelete({ id }: { id: number }) {
     onSuccess: () => {
       alert('게시글 삭제 성공');
       // 게시글 목록 캐시 무효화
-      queryClient.invalidateQueries({ queryKey: ['boards'] });
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.board.boards(category),
+      });
+      queryClient.refetchQueries({
+        queryKey: QUERY_KEY.board.boards(category),
+      });
     },
     onError: (error) => {
       console.error('게시글 삭제 실패:', error);
