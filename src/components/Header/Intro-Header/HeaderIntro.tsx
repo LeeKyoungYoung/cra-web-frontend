c0de5ca22be2e27c6e3f90e5dfb761fc95ecbdd7
JangSiYoung <chsy1894@handong.ac.kr>
2025-01-06 12:02:48 +0900

    [SY] feat[#66] : 헤더 수정

--- src/components/Header/Intro-Header/HeaderIntro.tsx
+++ src/components/Header/Intro-Header/HeaderIntro.tsx
@@ -4,14 +4,17 @@ import styles from './HeaderIntro.module.css';
 export default function HeaderIntro() {
   return (
     <div className={styles['header-intro']}>
-      <ul>
+      <ul className={styles['ul-intro']}>
         <Link to="/intro">
-          <li>
+          <li className={`${styles['li-intro']} ${styles['li-img']}`}>
             <img src="./cralogo.png" width="109" />
           </li>
         </Link>
-        <Link to="/main">
-          <li>main()</li>
+        <Link
+          to="/main"
+          className={`${styles['li-intro']} ${styles['navbar-link']}`}
+        >
+          main()
         </Link>
       </ul>
     </div>
