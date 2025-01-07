5b91971306a707af82e3aef48402b16a4a589a48
min06150315 <min06150315@gmail.com>
2025-01-07 10:27:42 +0900

    [KB] feat[#65]: ID/PW 찾기 페이지 CSS 및 라우팅, 하이퍼링크 설정

---
+++ src/pages/Login/Search/ID/IDSearchPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './IDSearchPage.module.css';
+
+export default function IDSearchPage() {
+  return (
+    <div className={styles['container']}>
+      <div>아이디 찾기 페이지입니다</div>
+      <div>추후 디자인 나오면 개발 예정</div>
+    </div>
+  );
+}
