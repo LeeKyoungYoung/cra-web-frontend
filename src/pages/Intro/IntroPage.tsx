dee37943b3058c25ce46156ba0a27b69da282060
min06150315 <min06150315@gmail.com>
2025-01-07 13:09:14 +0900

    [KB] feat[#63]: 인트로 페이지 크랑이 정상화

--- src/pages/Intro/IntroPage.tsx
+++ src/pages/Intro/IntroPage.tsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import HeaderIntro from '~/components/Header/Intro-Header/HeaderIntro';
-import ProjectIntro from './Project/ProjectIntro';
+import Vector from '~/assets/images/Arrow-Vector.png';
 import styles from './IntroPage.module.css';
 
 export default function IntroPage() {
   return (
-    <div className={styles['intro-container']}>
-      <div className={styles['intro-main']}>
+    <div className={styles['container']}>
+      <div className={styles['main']}>
         <HeaderIntro />
-        <div className={styles['intro-section']}>
+        <div className={styles['section']}>
           <div className={styles['banner']}>
             <div>LIVE YOUR</div>
             <div>PASSION</div>
@@ -19,10 +19,27 @@ export default function IntroPage() {
           <Link to="/recruit" className={styles['recruit-btn']}>
             <p>2025-1 CRA RECRUITMENT</p>
           </Link>
+          <div className={styles['vector']}>
+            <img src={Vector} />
+            <img src={Vector} />
+          </div>
         </div>
       </div>
-      <div className={styles['intro-section']}>
+      <div className={styles['cra']}>
+        <div className={styles['description']}>
+          <span>C</span>
+          <p>omputer </p>
+          <span>R</span>
+          <p>esearch </p>
+          <span>A</span>
+          <p>ssoication</p>
+        </div>
+      </div>
+      <div className={styles['club']}>
         <p>동아리 소개</p>
+      </div>
+      <div className={styles['project']}>
+        <p>프로젝트</p>
       </div>
     </div>
   );
