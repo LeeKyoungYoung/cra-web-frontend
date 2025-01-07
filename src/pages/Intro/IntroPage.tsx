d9d3cd52786b92d19ae72fe5ecd35eb87e0b81f9
min06150315 <min06150315@gmail.com>
2025-01-07 13:56:17 +0900

    [KB] feat[#63]: CRA 설명 디자인 정상화

--- src/pages/Intro/IntroPage.tsx
+++ src/pages/Intro/IntroPage.tsx
@@ -26,13 +26,37 @@ export default function IntroPage() {
         </div>
       </div>
       <div className={styles['cra']}>
-        <div className={styles['description']}>
+        <div className={styles['title']}>
           <span>C</span>
-          <p>omputer </p>
-          <span>R</span>
-          <p>esearch </p>
-          <span>A</span>
+          <p>omputer</p>
+          <span id={styles['cap-letter']}>R</span>
+          <p>esearch</p>
+          <span id={styles['cap-letter']}>A</span>
           <p>ssoication</p>
+        </div>
+        <div className={styles['description']}>
+          <p>CRA는 한동대학교 전산 교육과정에 기초하여</p>
+          <p>
+            한 분야에 국한되지 않는 신기술을 공부하고 습득한 지식과 기술을 통해
+          </p>
+          <p>
+            <span>‘배워서 남주자’</span>를 실천하는 동아리입니다.
+          </p>
+        </div>
+        <div className={styles['content']}>
+          <div className={styles['card']}>
+            <p id={styles['card-title']}>CRA가 창립한지</p>
+            <p id={styles['card-content']}>29년</p>
+          </div>
+          <div className={styles['card']}>
+            <p id={styles['card-title']}>출시 서비스</p>
+            <p id={styles['card-content']}>?개</p>
+          </div>
+          <div className={styles['card']}>
+            <p id={styles['card-title']}>선배들과 함께하는</p>
+            <p id={styles['card-content2']}>정기적인</p>
+            <p id={styles['card-content2']}>교류활동</p>
+          </div>
         </div>
       </div>
       <div className={styles['club']}>
