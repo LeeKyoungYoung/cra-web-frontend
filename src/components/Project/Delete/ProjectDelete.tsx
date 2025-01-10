188f2ef35e853d403fb359bd4afa1783085393e6
min06150315 <min06150315@gmail.com>
2025-01-10 20:24:55 +0900

    [KB] feat[#97]: 프로젝트 폴더 구조화

---
+++ src/components/Project/Delete/ProjectDelete.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ProjectDelete() {
+  return <div></div>;
+}
+
+export default ProjectDelete
---
+++ src/components/Project/Write/ProjectWrite.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ProjectWrite() {
+    return <div></div>;
+}
+
+export default ProjectWrite
