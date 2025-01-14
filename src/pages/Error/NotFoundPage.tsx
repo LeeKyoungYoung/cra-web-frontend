ad64656559bdf8e8141fafa77677db86a55645e7
min06150315 <min06150315@gmail.com>
2025-01-14 11:08:45 +0900

    [KB] feat[#103]: Archivo Black 폰트 적용

--- src/pages/Error/NotFoundPage.tsx
+++ src/pages/Error/NotFoundPage.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import CrangE from '~/assets/images/404_Crang.png';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   padding: 10rem;
+  p {
+    color: #2cb4db;
+  }
 `;
 
-const Status = styled.div``;
+const Status = styled.div`
+  display: flex;
+`;
+
+const StatusNum = styled.p`
+  font-family: 'Archivo Black';
+  font-size: 16.7rem;
+`;
+
+const Img = styled.img`
+  width: 15.75rem;
+  height: 14.875rem;
+`;
 
 const Content = styled.div``;
 
 const Buttons = styled.div``;
 
 function NotFoundPage() {
+  const navigate = useNavigate();
   return (
     <Container>
-      <Status>404</Status>
+      <Status>
+        <StatusNum>4</StatusNum>
+        <Img src={CrangE} />
+        <StatusNum>4</StatusNum>
+      </Status>
       <Content>
         <p>죄송합니다. 현재 찾을 수 없는 페이지를 요청하셨습니다.</p>
         <p>존재하지 않는 주소를 입력하셨거나,</p>
