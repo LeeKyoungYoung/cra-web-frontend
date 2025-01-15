1625d792531e74732cfbe16208e463685589bf27
min06150315 <min06150315@gmail.com>
2025-01-15 11:00:56 +0900

    [KB] feat[#82]: ID 찾기 페이지

--- src/pages/Login/ID/IDSearchPage.tsx
+++ src/pages/Login/ID/IDSearchPage.tsx
@@ -1,48 +1,74 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 10rem;
+  justify-content: center;
+  padding: 15rem 0;
+`;
+
+const Title = styled.h2`
+  line-height: 59px;
+  color: var(--color-bright-text);
+  font-size: 40px;
+  text-align: center;
+  margin-bottom: 70px;
 `;
 
 const Form = styled.form`
+  width: 100%;
+  max-width: 700px;
   background-color: var(--color-white);
-  width: 70%;
+  border-radius: 1rem;
+  padding: 3rem 4rem;
 `;
 
 const Input = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 0 auto;
+  margin-bottom: 2rem;
+
   label {
-    margin: 2rem 0rem 0rem 2rem;
+    color: var(--color-dark);
     font-family: 'Pretendard SemiBold';
-    transform: translateY(1.5rem);
+    font-size: 1.2rem;
+    margin-bottom: 0.5rem;
+    transform: translateY(1.25rem);
   }
   input {
-    border: 1px solid var(--color-dark-stroke);
+    width: 100%;
     background-color: var(--color-white);
-    padding: 1.5rem 1rem;
-    border-radius: 0.5rem;
     color: var(--color-dark-stroke);
+    border: 1px solid var(--color-dark-stroke);
+    border-radius: 0.5rem;
+    padding: 1.5rem 1rem;
+    transform: translateX(-2rem);
   }
 `;
 
 const Submit = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
   background-color: var(--color-primary);
   color: var(--color-white);
+  font-family: 'Pretendard Bold';
   border-radius: 1rem;
   border: none;
-  font-family: 'Pretendard Bold';
   padding: 1rem 10rem;
+  margin-bottom: 3rem;
+  cursor: pointer;
 `;
 
 function IDSearchPage() {
+  const navigate = useNavigate();
   return (
     <Container>
+      <Title>아이디 찾기</Title>
       <Form>
         <Input>
           <label htmlFor="studentNum">학번</label>
@@ -71,7 +97,14 @@ function IDSearchPage() {
             placeholder="이메일을 입력하세요."
           />
         </Input>
-        <Submit>확인</Submit>
+        <Submit
+          onClick={(e) => {
+            e.preventDefault();
+            navigate('/idsearch/complete');
+          }}
+        >
+          확인
+        </Submit>
       </Form>
     </Container>
   );
