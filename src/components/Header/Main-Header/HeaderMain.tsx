ffb3bafcf223fad2d13ba4fa60d0b5d1218dab3e
min06150315 <min06150315@gmail.com>
2025-01-17 17:39:07 +0900

    [KB] feat[#112]: Main Header에서 로그아웃 상태면 로그인 버튼이 뜨게 하고 로그인 상태면 로그아웃 버튼이 뜨게 하기 (추후에 내정보 버튼으로 수정)

--- src/components/Header/Main-Header/HeaderMain.tsx
+++ src/components/Header/Main-Header/HeaderMain.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import craIconBlue from '~/assets/images/cra-logo-blue.png';
+import { useAuthStore } from '~/store/authStore';
 import styles from './HeaderMain.module.css';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: var(--color-primary);
+  color: var(--color-white);
+  font-size: 1.5rem;
+  font-family: 'Pretendard Bold';
+  border: none;
+  border-radius: 0.5rem;
+  padding: 0.75rem 2rem;
+  cursor: pointer;
+`;
 
 export default function HeaderMain() {
+  const { isAuthenticated, logout } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
+  const handleLogout = () => {
+    try {
+      logout();
+      alert('로그아웃 성공');
+      navigate('/main');
+    } catch (error) {
+      alert('로그아웃 실패');
+    }
+  };
+
+  // const handleMyInfo = () => {
+  //   navigate('/info');
+  // };
   return (
     <div className={styles['header-main']}>
       <Link to="/main">
@@ -59,9 +92,16 @@ export default function HeaderMain() {
           </Link>
         </li>
       </ul>
-      <Link to="/login" className={`${styles['link']} ${styles['login']}`}>
-        로그인
-      </Link>
+      {isAuthenticated ? (
+        <>
+          <Button onClick={handleLogout}>로그아웃</Button>
+        </>
+      ) : (
+        <>
+          <Button onClick={handleLogin}>로그인</Button>
+        </>
+      )}
+
       {/* 로그인 하기 전에는 '로그인' Link이고, 로그인 이후에는 '내 정보' 버튼, 클릭 시 모달 출력력 */}
     </div>
   );
