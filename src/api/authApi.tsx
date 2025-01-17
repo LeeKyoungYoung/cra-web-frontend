272d4790995afa63736de1eb68076e97940b32c8
min06150315 <min06150315@gmail.com>
2025-01-17 18:09:51 +0900

    [KB] feat[#112]: Debuging을 위해서 코드 추가

--- src/api/authApi.tsx
+++ src/api/authApi.tsx
@@ -15,9 +15,14 @@ export const login = async (data: Login): Promise<ResponseToken> => {
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
-      console.error('Login Error:', error.response.data);
-      throw new Error(error.response.data?.message || 'Error during Login');
+      // 서버에서 반환한 메시지에 접근
+      const errorMessage =
+        error.response.data?.message || 'Unknown error during Login';
+      console.error('Login Error:', errorMessage);
+      throw new Error(errorMessage);
     } else {
+      // 예상치 못한 오류 처리
+      console.error('Unexpected Error:', error);
       throw new Error('Unexpected Error occurred during Login');
     }
   }
