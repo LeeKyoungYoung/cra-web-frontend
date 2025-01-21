c1acdaaf1a0d27f0adb41097719983595dd2ebe8
min06150315 <min06150315@gmail.com>
2025-01-21 11:09:31 +0900

    [KB] feat[#120]: 하브루타 api 불러오는 쿼리에서 과목 / 게시물로 세분화

---
+++ src/api/havruta/havrutaBoard.tsx
@@ -0,0 +1,119 @@
+import axios from 'axios';
+import { HavrutaBoard } from '~/models/Havruta';
+import { client } from '~/api/client';
+import { authClient } from '~/api/auth/authClient';
+
+// 하브루타 게시물 모두 가져오기
+export const getHavrutaBoards = async () => {
+  try {
+    const response = await client.get<HavrutaBoard[]>(`/board/havruta`);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Error fetching data: ${error.message}`);
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
+  }
+};
+
+// 과목별 하브루타 게시물 모두 가져오기
+export const getHavrutaBoardsByHavrutaId = async (id: number) => {
+  try {
+    const response = await client.get<HavrutaBoard>(`/board/havruta/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Error fetching data: ${error.message}`);
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
+  }
+};
+
+// 하브루타 게시물 상세보기
+export const getHavrutaBoardById = async (id: number) => {
+  try {
+    const response = await client.get<HavrutaBoard>(
+      `/board/havruta/view/${id}`,
+    );
+    const havruta = response.data;
+
+    return {
+      ...havruta,
+      createdAt: havruta.createdAt ? new Date(havruta.createdAt) : new Date(),
+    };
+  } catch (error) {
+    console.log(error);
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Error fetching data: ${error.message}`);
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
+  }
+};
+
+// 하브루타 게시물 작성하기
+export const createHavrutaBoard = async (havrutaBoard: HavrutaBoard) => {
+  try {
+    const response = await authClient.post<HavrutaBoard>(
+      '/board/havruta',
+      havrutaBoard,
+      {
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Failed to post data:', error);
+
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Axios error: ${error.message}`);
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
+  }
+};
+
+// 하브루타 게시물 수정하기
+export const updateHavrutaBoard = async (havrutaBoard: HavrutaBoard) => {
+  try {
+    const response = await authClient.put<HavrutaBoard>(
+      `/board/havruta/${havrutaBoard.id}`,
+      havrutaBoard,
+      {
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      },
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to update data:', error);
+
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Axios error: ${error.message}`);
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
+  }
+};
+
+// 하브루타 게시물 삭제하기
+export const deleteHavrutaBoards = async (id: number) => {
+  try {
+    const response = await authClient.delete(`/board/havruta/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Failed to delete havruta board:', error);
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Axios error: ${error.message}`);
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
+  }
+};
