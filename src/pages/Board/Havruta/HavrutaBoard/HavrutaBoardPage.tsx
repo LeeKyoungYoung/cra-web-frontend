46271c4c990bb59d1710b3ee0cf473aec00dfb73
min06150315 <min06150315@gmail.com>
2025-01-23 13:18:48 +0900

    [KB] feat[#124]: 하브루타 게시판 페이지에 적용 후 테스트 완료

--- src/pages/Board/Havruta/HavrutaBoard/HavrutaBoardPage.tsx
+++ src/pages/Board/Havruta/HavrutaBoard/HavrutaBoardPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import HavrutaBoardList from '~/components/Havruta/HavrutaBoard/List/HavrutaBoardList';
-
+import HavrutaBoardContainer from '~/components/Havruta/HavrutaBoard/List/HavrutaBoardContainer';
 export default function HavrutaBoardPage() {
   return (
     <div>
       <h1>Havruta 게시판</h1>
-      <HavrutaBoardList />
+      <HavrutaBoardContainer />
     </div>
   );
 }
