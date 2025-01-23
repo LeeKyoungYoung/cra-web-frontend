1e80b5f00a5a51ddb770aefaed0859bef4f77a4c
min06150315 <min06150315@gmail.com>
2025-01-23 13:17:12 +0900

    [KB] feat[#124]: 페이지네이션 컴포넌트 분리

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import LeftVector from '~/assets/images/Vector/LeftVector.png';
+import RightVector from '~/assets/images/Vector/RightVector.png';
+import styles from './Pagination.module.css';
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+function Pagination({
+  totalPages,
+  currentPage,
+  onPageChange,
+}: PaginationProps) {
+  return (
+    <div className={styles.pagenations}>
+      <img src={LeftVector} />
+      {[...Array(totalPages)].map((_, pageIndex) => (
+        <div
+          key={pageIndex}
+          className={`${styles['pagenations-elipse']} ${
+            currentPage === pageIndex
+              ? styles['pagenations-elipse-selected']
+              : styles['pagenations-elipse-unselected']
+          }`}
+          onClick={() => onPageChange(pageIndex)}
+        >
+          {pageIndex + 1}
+        </div>
+      ))}
+      <img src={RightVector} />
+    </div>
+  );
+}
+
+export default Pagination;
