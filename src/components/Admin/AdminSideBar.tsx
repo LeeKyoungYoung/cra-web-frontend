986898db030048d9495f645f480faddf27495d76
min06150315 <min06150315@gmail.com>
2025-02-03 16:36:09 +0900

    [KB] feat[#129]: 어드민 페이지 만들기

---
+++ src/components/Admin/AdminSideBar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Menu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(255, 255, 255);
+  border-radius: 1rem;
+`;
+
+const MenuItem = styled.li`
+  margin: 1rem 0;
+  border-radius: 10%;
+  a {
+    text-decoration: none;
+    font-family: 'Pretendard SemiBold';
+    font-size: 18px;
+    color: var(--color-brighter-text);
+    transition: color 0.5s;
+    &:hover {
+      color: var(--color-bright-text);
+    }
+  }
+`;
+
+export default function AdminSideBar() {
+  return (
+    <Menu>
+      <MenuItem>
+        <Link to="/admin">관리자 홈</Link>
+      </MenuItem>
+      <MenuItem>
+        <Link to="/admin/book">도서 관리</Link>
+      </MenuItem>
+      <MenuItem>
+        <Link to="/admin/item">물품 관리</Link>
+      </MenuItem>
+      <MenuItem>
+        <Link to="/admin/havruta">하브루타 과목 관리</Link>
+      </MenuItem>
+      <MenuItem>
+        <Link to="/admin/project">프로젝트 관리</Link>
+      </MenuItem>
+    </Menu>
+  );
+}
