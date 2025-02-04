1337258a8ee14b27486d84e2a5b1a29c28d63c75
JangSiYoung <chsy1894@handong.ac.kr>
2025-02-04 14:11:37 +0900

    [SY] feat[#59]: merge

--- src/components/Comment/Edit/CommentEdit.tsx
+++ src/components/Comment/Edit/CommentEdit.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { updateComments } from '~/api/comment';
 
