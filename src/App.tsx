73d1b8de05314e2534cc67c341eb7a99cef162c8
min06150315 <min06150315@gmail.com>
2025-02-04 17:02:12 +0900

    [KB] chore[#156]: module css 적용 방법 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,17 @@ import Footer from './components/Footer/Footer.tsx';
 import Header from './components/Header/Header.tsx';
 import ScrollToTop from './utils/ScrollToTop.tsx';
 import { RouterProvider } from '@tanstack/react-router';
-import './App.css';
+import styles from './App.module.css';
 import { routes } from './routes/routes.ts';
 import { Suspense } from 'react';
-const Loding = {
-  padding: '10rem',
-};
 
 function App() {
   return (
-    <div className="app-container">
+    <div className={styles.appContainer}>
       <Header />
-      <div className="contents">
+      <div className={styles.contents}>
         <ScrollToTop />
-        <Suspense fallback={<div style={Loding}>Loading...</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
           <RouterProvider router={routes} />
         </Suspense>
       </div>
